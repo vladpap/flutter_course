b83e5070a7d47b672cc750e305f0dd45e3cea408
Vladimir Papin <vladimir.papin@gmail.com>
2025-12-24 09:53:47 +0300

    added HomeWidget, icons app

--- lib/main.dart
+++ lib/main.dart
@@ -26,25 +26,35 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Flutter application"),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                "assets/images/roll.png",
-                width: 250,
-              ),
-              const Text(
-                "Вкусные роллы",
-                style: TextStyle(fontSize: 24.0),
-                ),
-              ElevatedButton(onPressed: (){
-                print ("Вы купили роллы. Спасибо за заказ!");
-              }, 
-              child: Text("Купить")),
-            ],
+        body: HomeWidget(),
+      ),
+    );
+  }
+}
+
+class HomeWidget extends StatelessWidget {
+  const HomeWidget
+({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            "assets/images/roll.png",
+            width: 250,
           ),
-        ),
+          const Text(
+            "Вкусные роллы",
+            style: TextStyle(fontSize: 24.0),
+            ),
+          ElevatedButton(onPressed: (){
+            print ("Вы купили роллы. Спасибо за заказ!");
+          }, 
+          child: Text("Купить")),
+        ],
       ),
     );
   }
