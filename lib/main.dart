04da3ef3a346b06ec817e689d56e2ec5920d2080
Vladimir Papin <vladimir.papin@gmail.com>
2025-12-25 12:22:42 +0300

    Learn Column Row Container

--- lib/main.dart
+++ lib/main.dart
@@ -9,51 +9,63 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    var myColorScheme = ColorScheme.fromSeed(seedColor: Colors.purple);
-
     return MaterialApp(
-      title: "Flutter app",
-
+      debugShowCheckedModeBanner: false,
+      title: "Hello Flutter App",
       theme: ThemeData(
-        colorScheme: myColorScheme,
-        appBarTheme: AppBarTheme(
-          backgroundColor: myColorScheme.inversePrimary,
-        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Flutter application"),
-        ),
-        body: HomeWidget(),
-      ),
+      home: HomeWidget(),
     );
   }
 }
 
 class HomeWidget extends StatelessWidget {
-  const HomeWidget
-({super.key});
+  const HomeWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    // 👉 Scaffold теперь здесь!
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true, 
+        title: const Text("UI Column Row"),
+      ),
+      body: Center(
+        child: RollCard() // 👉 Передаём виджет с карточкой
+      ),
+    );
+  }
+}
+
+class RollCard extends StatelessWidget {
+  const RollCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200, // 👉 Высота контейнера
+      padding: const EdgeInsets.all(16), // 👉 Внутренний отступ
+
+      decoration: BoxDecoration(
+        color: Colors.white, // 👉 Цвет фона белый
+        border: Border.all(color: Colors.blueGrey), // 👉 Границы черные
+        borderRadius: BorderRadius.circular(16), // 👉 Закругление
+        boxShadow: [BoxShadow(
+          color: Colors.grey,
+          offset: Offset(2, 2),
+          blurRadius: 5,
+        ),],
+      ),
+
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image.asset(
-            "assets/images/roll.png",
-            width: 250,
-          ),
-          const Text(
-            "Вкусные роллы",
-            style: TextStyle(fontSize: 24.0),
-            ),
-          ElevatedButton(onPressed: (){
-            print ("Вы купили роллы. Спасибо за заказ!");
-          }, 
-          child: Text("Купить")),
+          Image.asset("assets/images/roll.png", width: 100),
+          SizedBox(height: 10),
+          Text("Вкусные Роллы", style: TextStyle(fontSize: 14)),
+          SizedBox(height: 10),
+          ElevatedButton(onPressed: () {}, child: Text("Купить")),
         ],
       ),
     );
