a97a528c08d8da3abe7c06c570a4a0795f0cb7c6
Vladimir Papin <vladimir.papin@gmail.com>
2026-01-08 16:42:07 +0300

    added category

--- lib/practical/practical_3.dart
+++ lib/practical/practical_3.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_cource/practical/category.dart';
 
 
 class PracticLayout3 extends StatelessWidget {
@@ -10,6 +11,7 @@ class PracticLayout3 extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xFFF7F7F9),
       appBar: AppBar(
+        toolbarHeight: 80,
         actionsPadding: EdgeInsets.only(right: 18),
         backgroundColor: Color(0xFFF7F7F9),
         leading: IconButton(
@@ -20,24 +22,28 @@ class PracticLayout3 extends StatelessWidget {
           onPressed: () {}, 
           ),
         centerTitle: true,
-        title: const Text(
-          "Главная",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-          ),
-        actions: [ShopCart(empty_cart: false)],
+        title: Text(
+            "Главная", style: TextStyle(fontSize: 36)),
+        actions: [ShopCart(emptyCart: false)],
       ),
-      body: Text("data 12dfgfggh3"),
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            CategoryChart(),
+            Text("Populyar"),
+            Text("Action"),
+          ],
+        ),
+      ),
     );
   }
 }
 
 class ShopCart extends StatelessWidget {
-  final bool empty_cart;
+  final bool emptyCart;
   const ShopCart({
-    required this.empty_cart,
+    required this.emptyCart,
     super.key
     });
 
@@ -65,7 +71,7 @@ class ShopCart extends StatelessWidget {
             ),
           onPressed: () {},
         ),
-        if (!empty_cart) 
+        if (!emptyCart) 
           // Red circle
           Positioned(
             top: 3,
